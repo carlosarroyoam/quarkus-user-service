024dd14dd5e50a97c99cd673f58c34607befa3d5
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2023-07-22 13:15:44 -0600

    Agrega mapper para Exception.class

--- src/main/java/com/carlosarroyoam/userservice/exception/ExceptionMapper.java
+++ src/main/java/com/carlosarroyoam/userservice/exception/ExceptionMapper.java
@@ -3,6 +3,7 @@ package com.carlosarroyoam.userservice.exception;
 import java.time.Clock;
 import java.time.ZonedDateTime;
 
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
 import com.carlosarroyoam.userservice.dto.AppExceptionDto;
@@ -10,12 +11,13 @@ import com.carlosarroyoam.userservice.dto.AppExceptionDto;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.core.UriInfo;
 
 class ExceptionMappers {
 
+	private static final Logger LOG = Logger.getLogger(ExceptionMappers.class);
 	private final UriInfo uriInfo;
-
 	private final Clock clock;
 
 	@Inject
@@ -26,7 +28,7 @@ class ExceptionMappers {
 	}
 
 	@ServerExceptionMapper
-	public Response mapException(WebApplicationException ex) {
+	public Response mapWebApplicationException(WebApplicationException ex) {
 		AppExceptionDto appExceptionDto = new AppExceptionDto();
 		appExceptionDto.setMessage(ex.getMessage());
 		appExceptionDto.setCode(ex.getResponse().getStatus());
@@ -34,7 +36,23 @@ class ExceptionMappers {
 		appExceptionDto.setPath(uriInfo.getPath());
 		appExceptionDto.setTimestamp(ZonedDateTime.now(clock));
 
+		LOG.error(ex.getMessage());
+
 		return Response.status(ex.getResponse().getStatus()).entity(appExceptionDto).build();
+	}
+
+	@ServerExceptionMapper
+	public Response mapException(Exception ex) {
+		AppExceptionDto appExceptionDto = new AppExceptionDto();
+		appExceptionDto.setMessage("Whoops! Something went wrong");
+		appExceptionDto.setCode(Status.INTERNAL_SERVER_ERROR.getStatusCode());
+		appExceptionDto.setStatus(Status.INTERNAL_SERVER_ERROR.getReasonPhrase());
+		appExceptionDto.setPath(uriInfo.getPath());
+		appExceptionDto.setTimestamp(ZonedDateTime.now(clock));
+
+		LOG.error(ex.getMessage());
+
+		return Response.status(Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(appExceptionDto).build();
 	}
 
 }
