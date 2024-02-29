800b316826181f13a5c9a02be31bd5da448899e8
carroyom <carroyo@emeal.nttdata.com>
2024-02-29 12:35:37 -0600

    Cambia body a Map

--- src/test/java/com/carlosarroyoam/userservice/resource/AuthResourceTest.java
+++ src/test/java/com/carlosarroyoam/userservice/resource/AuthResourceTest.java
@@ -5,9 +5,12 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
-import com.carlosarroyoam.userservice.dto.LoginRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
@@ -17,23 +20,23 @@ import jakarta.ws.rs.core.Response.Status;
 class AuthResourceTest {
 
 	@Test
-	void shouldReturnOkWhenAuthUserWithValidCredentials() {
-		LoginRequest loginRequest = new LoginRequest();
-		loginRequest.setUsername("carroyom");
-		loginRequest.setPassword("secret");
+	void shouldReturnOkWhenAuthUserWithValidCredentials() throws JsonProcessingException {
+		Map<String, Object> body = new HashMap<>();
+		body.put("username", "carroyom");
+		body.put("password", "secret");
 
-		given().contentType(ContentType.JSON).body(loginRequest).when().post("/api/v1/auth/signin").then()
+		given().contentType(ContentType.JSON).body(body).when().post("/api/v1/auth/signin").then()
 				.statusCode(Status.OK.getStatusCode()).body("username", is(not(nullValue())))
 				.body("access_token", is(not(nullValue())));
 	}
 
 	@Test
 	void shouldReturnUnauthorizedWhenAuthUserWithNonValidCredentials() {
-		LoginRequest loginRequest = new LoginRequest();
-		loginRequest.setUsername("nonExist");
-		loginRequest.setPassword("nonExist");
+		Map<String, Object> body = new HashMap<>();
+		body.put("username", "nonExist");
+		body.put("password", "nonExist");
 
-		given().contentType(ContentType.JSON).body(loginRequest).when().post("/api/v1/auth/signin").then()
+		given().contentType(ContentType.JSON).body(body).when().post("/api/v1/auth/signin").then()
 				.statusCode(Status.UNAUTHORIZED.getStatusCode());
 	}
 
